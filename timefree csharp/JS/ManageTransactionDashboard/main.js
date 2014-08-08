8043379d484e33af88b72271a9e7ac2a5a0deda4
jankenlim <kendrick.lim@delasalle.ph>
2014-08-08 17:02:04 +0800

    Transaction Dashboard optimization

--- timefree csharp/JS/ManageTransactionDashboard/main.js
+++ timefree csharp/JS/ManageTransactionDashboard/main.js
@@ -3,42 +3,12 @@
     $('#ManageTransactionTable').dataTable({
         bFilter: false,
         bInfo: false,
-        "iDisplayLength": 5
+        "iDisplayLength": 5,
+        "aoColumnDefs": [
+          { 'bSortable': false, 'aTargets': [-1] }
+        ]
     });
-
-    imagePreview();
 });
-
-this.imagePreview = function () {
-    /* CONFIG */
-
-    xOffset = 10;
-    yOffset = 30;
-
-    // these 2 variable determine popup's distance from the cursor
-    // you might want to adjust to get the right result
-
-    /* END CONFIG */
-    $("img.preview").hover(function (e) {
-        this.t = this.title;
-        this.title = "";
-        var c = (this.t != "") ? "<br/>" + this.t : "";
-        $("body").append("<p id='preview'><img src='" + this.href + "' alt='Image preview' />" + c + "</p>");
-        $("#preview")
-			.css("top", (e.pageY - xOffset) + "px")
-			.css("left", (e.pageX + yOffset) + "px")
-			.fadeIn("fast");
-    },
-	function () {
-	    this.title = this.t;
-	    $("#preview").remove();
-	});
-    $("img.preview").mousemove(function (e) {
-        $("#preview")
-			.css("top", (e.pageY - xOffset) + "px")
-			.css("left", (e.pageX + yOffset) + "px");
-    });
-};
 
 $(function () {
     $('#side-menu').metisMenu();
